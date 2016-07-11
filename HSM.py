e5e93ed39aaac6701104ee3da7c6761d2b76e9fb
pieceofduke <pieceofduke@gmail.com>
2016-07-11 19:53:28 +0300

    Ported to json
    Changed the set files format to json.
    Functionality unchanged

--- HSM.py
+++ HSM.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Teaching machine by Holland and Skinner. Behavior analysis self-teaching.
-import sys, os
+import sys, os, json
 
 def mylistdir(directory):
     """A specialized version of os.listdir() that ignores files that
@@ -15,24 +15,15 @@ def display_welcome():
     print()
     print("Welcome to Holland-Skinner Teaching Machine!\n".center(80))
 
-def line_by_prefix(filepath, prefix):
-    """takes file path and a line prefix
-       finds the first occurence of prifix in the file
-       returns the rest of the line (omitting the prefix)"""
-    with open(filepath) as f:
-        for line in f.readlines():
-            if line.startswith("#"):        #ignore comments in file
-                next
-            if line.startswith(prefix):
-                return line[len(prefix):-1] #strip off trailing \n
-
 def display_contents(course_folder):
     """Prints course contents"""
     print("Course contents:")
     for setname in mylistdir(course_folder):
         setpath = os.path.join(course_folder, setname)
-        print(line_by_prefix(setpath, "!set_number: "), end = ' ')
-        print(line_by_prefix(setpath, "!set_name: "), )
+        with open(setpath) as f:
+            the_set = json.loads(f.read())
+            print(the_set['set_number'], end = " ")
+            print(the_set["set_name"])
 
 def main():
     """Main entry point for the script"""
