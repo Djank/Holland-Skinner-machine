b4afbb5a4630a6be6b5345bcfa197be693ea3232
pieceofduke <pieceofduke@gmail.com>
2016-07-13 14:10:09 +0300

    Curriculum + progress
    Implemented curriculum construction and the progress file.
    ToDo: displaying the frames loop.

--- HSM.py
+++ HSM.py
@@ -3,41 +3,73 @@
 import sys, os, json
 
 def mylistdir(directory):
-    """A specialized version of os.listdir() that ignores files that
-    start with a leading period."""
+    """ A specialized version of os.listdir() that ignores files that
+    start with a leading period """
     filelist = os.listdir(directory)
     return [x for x in filelist
             if not (x.startswith('.'))]
 
-
 def display_welcome():
-    """print welcome line on launch of program"""
+    """ print welcome line on launch of program """
     print()
     print("Welcome to Holland-Skinner Teaching Machine!\n".center(80))
 
-def display_contents(course_folder):
-    """Prints course contents"""
+def display_contents(course_folder, current_set_num):
+    """ prints course contents and prompts the user to quit or proceed """
     print("Course contents:")
-    for setname in mylistdir(course_folder):
-        setpath = os.path.join(course_folder, setname)
-        with open(setpath) as f:
-            the_set = json.loads(f.read())
-            print(the_set['set_number'], end = " ")
-            print(the_set["set_name"])
+    curriculum = {} # {set-number: path-to-set} for future use
+    for set_filename in mylistdir(course_folder):
+        set_path = os.path.join(course_folder, set_filename)
+        with open(set_path) as f:
+            this_set = json.loads(f.read())
+            this_set_name = this_set["set_name"]
+            this_set_num = this_set["set_number"]
+            print(this_set_num, end = " ")
+            if this_set_num == current_set_num:
+                print(this_set_name.upper() + \
+                      "<-- current set".rjust(65 - len(this_set_name)))
+            else:
+                print(this_set_name)
+            curriculum[this_set_num] = set_path
+#    if input("[q]uit or [enter]\n") == 'q':
+#        return 'User quit'
+#    else:
+    return curriculum
+
+def get_progress(progress_file_path):
+    """ returns user progress """
+    try:
+        with open(progress_file_path, 'x') as f:
+            initial = '{"current set": "1.1", "frames complete": []}'
+            f.write(initial)
+        return json.loads(initial)
+    except FileExistsError:
+        with open(progress_file_path, 'r') as f:
+            return (json.loads(f.read()))
+
+def display_frame(progress, curriculum):
+    """ displays the frame interface """
+    # TODO
+    pass
 
 def main():
-    """Main entry point for the script"""
-    # HSM.py file path
+    """ Main entry point for the script """
+    # HSM.py file path:
     hsm_folder_path = (os.path.split(os.path.realpath(__file__))[0])
-
-    # resources folder path
+    # resources folder path:
     resources_path = os.path.join(hsm_folder_path, "resources")
-
-    # course content files folder path
+    # course content files folder path:
     course_folder_path = os.path.join(resources_path, "course")
-
+    # user progress file path:
+    progress_file_path = os.path.join(resources_path, "progress.json")
     display_welcome()
-    display_contents(course_folder_path)
+    progress = get_progress(progress_file_path)
+    curriculum = display_contents(course_folder_path, progress["current set"])
+    if curriculum == 'User quit':
+        return 'User quit'
+    display_frame(progress, curriculum)
+    
+    return "Normal exit"
 
 if __name__ == '__main__':
-        sys.exit(main())
+    sys.exit(print(main()))
